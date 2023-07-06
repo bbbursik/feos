dec99b8bff37e0afd1850c541154a27108fdfa8c
Benjamin Bursik <benjamin.bursik@itt.uni-stuttgart.de>
2023-07-06 18:58:20 +0200

    change lambda fix in pure saft functional

--- src/pcsaft/dft/pure_saft_functional.rs
+++ src/pcsaft/dft/pure_saft_functional.rs
@@ -190,7 +190,7 @@ impl<N: DualNum<f64> + Copy + ScalarOperand> FunctionalContributionDual<N> for P
 
         lambda.iter_mut().zip(rho.into_iter()).for_each(|(l, &d)| {
             if l.re() < 1e-8 {
-                *l = d + N::from(f64::EPSILON);
+                *l = d;// + N::from(f64::EPSILON);
                 // println!("Using lambda=rho for small densities in chain functional ");
             }
         });
